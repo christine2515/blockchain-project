908603cc79c8e1ee9650ca5fb61910f89316fc86
maya-maye <95601135+maya-maye@users.noreply.github.com>
2022-05-26 18:07:49 -0700

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,12 +3,12 @@ import react from "react"
 const About = () => {
     return (
         <div>
-            <h1 className = "questionheader">Rules for the Game!</h1>
-            <ul>
+            <h1 className = "about">Rules for the Game!</h1>
+            <h1 className= "rules">
                 <li>   Everyday a new yes/no question will appear on the home page</li>
                 <li>   You can vote once per day, through your account, inputting an amount to bet as well</li>
                 <li>   Whichever side gets the minority number of votes. If you win, you get your earnings doubled!</li>
-            </ul>
+            </h1>
         </div>
     )
 }
